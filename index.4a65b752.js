16aabdfce794ac3cbe4a645351da73542147c54d
Apophis96 <150725771+Apophis96@users.noreply.github.com>
2024-04-07 10:30:17 +0000

    Deploying to gh-pages from @ Apophis96/Team-4-ice-cream@bbec9358cf3fba2b45320befda646507fc20b506 🚀

--- index.4a65b752.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",pagination:{clickable:!0,el:".testimonial-dots"}});
-//# sourceMappingURL=index.4a65b752.js.map
--- index.a87854c9.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",pagination:{clickable:!0,el:".testimonial-dots"}});
-//# sourceMappingURL=index.a87854c9.js.map
